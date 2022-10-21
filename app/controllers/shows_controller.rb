1dbac70201c95008ee8c2f059a839e4cda32281f
Achalles <achalles32@aceproject.space>
2022-10-20 20:23:14 -0500

    adds Hierarchical Collection Navigation to characters and shows show pages.

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,5 +5,6 @@ class ShowsController < ApplicationController
 
   def show
     @show = Show.find(params[:id])
+    @characters = Character.where("show_id LIKE ?", params[:id]).order(name: :asc).page(params[:page])
   end
 end
